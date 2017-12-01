da4bbfaa0b66a02d1ce98d075a551a41196fff50
shalarewicz <shalarewicz@gmail.com>
2017-12-01 15:07:01 -0500

    Added method Vertex.getWeight() and modified Vertex.setTarget() to return previous weight. Implemented and corrected tests for vertex class.

--- src/graph/ConcreteVerticesGraph.java
+++ src/graph/ConcreteVerticesGraph.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
 
 /**
  * An implementation of Graph.
@@ -18,7 +19,7 @@ public class ConcreteVerticesGraph implements Graph<String> {
     private final List<Vertex> vertices = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    //   Each vertex is a node in the Graph.
     // Representation invariant:
     //   TODO
     // Safety from rep exposure:
@@ -57,30 +58,109 @@ public class ConcreteVerticesGraph implements Graph<String> {
 }
 
 /**
- * TODO specification
  * Mutable.
  * This class is internal to the rep of ConcreteVerticesGraph.
  * 
- * <p>PS2 instructions: the specification and implementation of this class is
- * up to you.
+ * <p> Vertex is a mutable structure. Each vertex has a name as well as 0 or more targets which are
+ * connected to this vertex by and edge with a weight greater than zero. 
  */
 class Vertex {
     
-    // TODO fields
+    private Map<Vertex, Integer> targets = new HashMap<Vertex, Integer>();
+    private String name;
     
     // Abstraction function:
-    //   TODO
+    //   Vertex consists of a name and a map mapping each target where vertex is the source to the weight of the edge between 
+    // 	 source and vertex. 
     // Representation invariant:
-    //   TODO
+    //   Weight not equal to zero
+    // 	 Name not null
     // Safety from rep exposure:
     //   TODO
     
-    // TODO constructor
     
-    // TODO checkRep
+    /**
+     * Constructor for nested class Vertex
+     * @param name - Creates vertex with name - name and now targets
+     */
+    public Vertex(String name) {
+    	this.name = name;
+    }
+
     
-    // TODO methods
+    private void checkRep() {
+    	assert name != null;
+    	for (Vertex target : targets.keySet()) {
+    		assert targets.get(target) != 0;
+    	}
+    }
+    
+    //Setters
+    /**
+     * Changes the name of the current vertex to name
+     * @param name - The new name of vertex
+     */
+    public void setName(String name) {
+    	this.name = name;
+    	checkRep();
+    }
+    
+    /**
+     * If weight > 0, adds an edge in the graph between this vertex and the target with weight weight. If the vertex already has this target
+     * the weight is updated. If this edge did not exist the edge is added. If weight = 0, the edge is removed.
+     * @param name - Name of the target
+     * @param weight - Weight of edge from this vertex to target. Requires weight >= 0;
+     * @return - Weight of the previous edge between the two vertices or 0 if no such edge existed.
+     */
+    public int setTarget(Vertex target, int weight) {
+    	int oldWeight = getWeight(target);
+    	if (weight > 0) {
+    		targets.put(target, weight);
+    	} else {
+    		targets.remove(target);
+    	}
+    	checkRep();
+    	return oldWeight;  		
+    }
+    
+    //Getters
+    
+    /**
+     * 
+     * @return Returns the name of the vertex
+     */
+    public String getName() {
+    	return this.name;
+    }
+    /**
+     * Return the weight of the edge between the this vertex and one of it's target vertices. If no edge exists between 
+     * these vertices 0 is returned
+     * @param target - Target vertex of the edge
+     * @return - Weight of the edge or 0 if edge does not exist
+     */
+    public int getWeight(Vertex target) {
+    	return this.targets.getOrDefault(target, 0);
+    }
+    
+    /**
+     * 
+     * @return Returns a set of targets of this vertex. 
+     */
+    public Set<Vertex> getTargets(){
+    	return this.targets.keySet();
+    }
+    
+    @Override
+    public String toString() {
+    	// Source=[target: weight, target, weight]
+    	StringBuilder result = new StringBuilder();
+    	
+    	return result.toString();
+    }
+    
     
     // TODO toString()
     
 }
+
+	
--- test/graph/ConcreteVerticesGraphTest.java
+++ test/graph/ConcreteVerticesGraphTest.java
@@ -5,6 +5,11 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
 /**
@@ -38,8 +43,93 @@ public class ConcreteVerticesGraphTest extends GraphInstanceTest {
      */
     
     // Testing strategy for Vertex
-    //   TODO
+   /*
+    * setName - current name has length 0 and max length?
+    * setTarget - vertex has 0, n targets
+    * 			- Weight is changed, not changed. 
+    * getName - current name has length 0 and max length?
+    * getTargets - vertex has 0, 1, n targets
+    * toString - vertex has 0, 1, n targets
+    * getWeight - Weight 0, n
+    */
+    
+    
+    // Test Vertices
+    
+    private final Vertex V1 = new Vertex("Test1");
+    private final Vertex V2 = new Vertex("Test2");
+    private final Vertex V3 = new Vertex("Test3");
+    private final Vertex V4 = new Vertex("Test4");
+    private final int WEIGHT5 = 5;
+    private final int WEIGHT4 = 4;
+    private final int WEIGHT0 = 0;
     
     // TODO tests for operations of Vertex
+    @Test
+    // test setName
+    public void testSetName() {
+    	String expected = "Apple 84 &";
+    	V1.setName(expected);
+    	assertEquals("expected vertex with name ", expected, V1.getName());
+    }
+    
+    @Test
+    // tests setTarget when 0, n existing targets
+    // tests getTargets when 1, n targets
+    public void testSetNewTarget() {
+    	assertEquals("expected weight ", WEIGHT0, V1.setTarget(V2, WEIGHT5));
+    	assertEquals("expected to have targets ", Collections.singleton(V2), V1.getTargets());
+    	V1.setTarget(V3, WEIGHT4);
+    	Set<Vertex> expected = new HashSet<Vertex>(Arrays.asList(V2, V3));
+    	assertEquals("expected to have targets ", expected, V1.getTargets());
+    }
+    @Test
+    // test setTarget when weight is changed
+    // tests getWeight when weight = 0, n
+    public void testSetTargetChangeWeight() {
+    	assertEquals("expected weight ", WEIGHT0, V1.getWeight(V2));
+    	assertEquals("expected weight ", WEIGHT0, V1.setTarget(V2, WEIGHT5));
+    	assertEquals("expected weight ", WEIGHT5, V1.setTarget(V2, WEIGHT4));
+    	assertEquals("expected weight ", WEIGHT4, V1.getWeight(V2));
+    }
+    
+    @Test
+    // Tests setTarget when edge to be created has weight 0;
+    // Tests getTargets when no targets exist
+    public void testSetTargetWeightZero() {
+    	V1.setTarget(V4, WEIGHT5);
+    	V1.setTarget(V4, WEIGHT0);
+    	assertEquals("expected to have no targets", Collections.EMPTY_SET, V1.getTargets());
+    }
+    
+    @Test
+    // Tests getName
+    public void testgetName() {
+    	assertEquals("expected name ", "Test1", V1.getName());
+    }
+    
+    @Test
+    // tests toString when no targets
+    public void testToStringNoTargers() {
+    	String expected = "Test2=[]";
+    	assertEquals("expected string ", expected, V1.toString());
+    }
+    
+    @Test
+    // tests toString when 1 target
+    public void testToStringOneTarget() {
+    	V1.setTarget(V4, WEIGHT5);
+    	String expected = "Test2=[Test4: 5]";
+    	assertEquals("expected string ", expected, V1.toString());
+    }
+    
+    @Test
+    // tests toString when n targets
+    public void testToStringManyTargets() {
+    	V1.setTarget(V4, WEIGHT5);
+    	V1.setTarget(V3, WEIGHT4);
+    	String expected = "Test2=[Test3: 4, Test4: 5]";
+    	assertEquals("expected string ", expected, V1.toString());
+    }
     
 }
